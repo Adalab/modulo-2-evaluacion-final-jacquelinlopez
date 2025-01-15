92ac1eec8392c5576b6ee3a51cf4b2b4aafdb6d5
María del Carmen <jacqueelopez@gmail.com>
2025-01-15 21:01:06 +0100

    created functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
 'use strict';
+//Traigo a mis elementos del html para Busqueda//
 
-console.log('>> Ready :)');
+const inputSearch = document.querySelector('.js-search');
+const btn = document.querySelector('.js-btn');
+const reset = document.querySelector('.js-reset');
+const listResults = document.querySelector('.js-results');
+const listFavs = document.querySelector('.js-favs');
+let arraySeries = [];
+let arrayFav = [];
+
+
+
+ /*1-Busqueda de series ---> 
+  -Escuho evento sobre el boton buscar--lo meto dentro de una funcion.  
+  -Agregar prevent default.
+  
+  */
+
+ function handleSearch(ev){
+    ev.preventDefault(); //prevent del boton//
+    const inputValue = input.value;//guardo valor input//
+
+    fetch('https://api.jikan.moe/v4/anime?q=${inputValue}')
+    .then((resp)=> resp.json())
+    .then((data)=>{
+        const animeApi = data.data;
+
+        arraySeries=[];
+
+        //bucle sobre el array para obtener datos 
+
+        for(const serie of animeApi) {
+
+        }
+    });
+
+
+
+ }
+
