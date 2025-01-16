3c7a28243935e695f394d1235dc56fdd509af78a
María del Carmen <jacqueelopez@gmail.com>
2025-01-16 09:03:48 +0100

    render series ok

--- src/js/main.js
+++ src/js/main.js
@@ -27,41 +27,45 @@ let arrayFav = [];
 
   //Escuchamos el evento y le decimos al servidor que nos de una lista que contenga el input que se ingreso//
 
-  /funcion para pintar resultados//
-  function renderResults(){
-     for(const series of arraySeries){
-         let image =series.images.jpg.image_url;
- 
-         if(image === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png" ) {
-             image = "https://placehold.co/600x400"
-         }
- 
-         //añado la lista a mi html//
-         listResults.innerHTML += `<li class="list js-anime" id=${series.mal_id}>  
-             <h5>${series.title}</h5>
-             <img src="${image}" class="image" alt="${series.title} "/>
-             </li>`
- 
-             const allAnime = document.querySelectorAll('.js-anime');
-             for (const anime of allAnime){
-                 anime.addEventListener('click',handleAddFavorites);
-             }
-     }
-  }
- 
-       
-
-
  function handleClick(ev){
     ev.preventDefault(); //prevent del boton//
     const inputText = inputSearch.value;//guardo valor input//
+   
 
     fetch(`https://api.jikan.moe/v4/anime?q=${inputText}`)
-    .then(resp => resp.json())
-    .then(info =>{
-        const seriesApi = info.data;
-        renderResults();
-    })
-}
+    .then((resp) => resp.json())
+    .then((info) =>{
+         arraySeries = info.data;
+        renderResults(arraySeries);
+    });
+};
+
 btn.addEventListener('click', handleClick);
+
+
+//funcion para pintar resultados//
+function renderResults(series){
+    listResults.innerHTML = '';
+   for(const serie of series){
+    
+       let image =serie.images.jpg.image_url;
+
+       if(image === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png" ) {
+           image = "https://placehold.co/600x400"
+       }
+
+       //añado la lista a mi html//
+       listResults.innerHTML += `<li class="list js-anime" id=${series.mal_id}>  
+           <h5>${series.title}</h5>
+           <img src="${image}" class="image" alt="${series.title} "/>
+           </li>`
+
+           /*const allAnime = document.querySelectorAll('.js-anime');
+           for (const anime of allAnime){
+               anime.addEventListener('click',handleAddFavorites);
+           }*/
+   }
+}
+
+     
 
