8fad89c1946e33feb96b15db7478492385ade6d4
María del Carmen <jacqueelopez@gmail.com>
2025-01-16 08:20:23 +0100

    readme

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,67 @@
 'use strict';
+
 //Traigo a mis elementos del html para Busqueda//
 
 const inputSearch = document.querySelector('.js-search');
 const btn = document.querySelector('.js-btn');
 const reset = document.querySelector('.js-reset');
+
 const listResults = document.querySelector('.js-results');
-const listFavs = document.querySelector('.js-favs');
+
+let listFavs = document.querySelector('.js-favs');
+
+//arrays para guardar 
 let arraySeries = [];
 let arrayFav = [];
 
 
 
  /*1-Busqueda de series ---> 
-  -Escuho evento sobre el boton buscar--lo meto dentro de una funcion.  
+  -Escucho evento sobre el boton buscar--lo meto dentro de una funcion.  
   -Agregar prevent default.
+  -Fetch
+  -Pintar resultados 
+  -Bucle recorriendo array
   
   */
 
- function handleSearch(ev){
+  //Escuchamos el evento y le decimos al servidor que nos de una lista que contenga el input que se ingreso//
+
+  /funcion para pintar resultados//
+  function renderResults(){
+     for(const series of arraySeries){
+         let image =series.images.jpg.image_url;
+ 
+         if(image === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png" ) {
+             image = "https://placehold.co/600x400"
+         }
+ 
+         //añado la lista a mi html//
+         listResults.innerHTML += `<li class="list js-anime" id=${series.mal_id}>  
+             <h5>${series.title}</h5>
+             <img src="${image}" class="image" alt="${series.title} "/>
+             </li>`
+ 
+             const allAnime = document.querySelectorAll('.js-anime');
+             for (const anime of allAnime){
+                 anime.addEventListener('click',handleAddFavorites);
+             }
+     }
+  }
+ 
+       
+
+
+ function handleClick(ev){
     ev.preventDefault(); //prevent del boton//
-    const inputValue = input.value;//guardo valor input//
+    const inputText = inputSearch.value;//guardo valor input//
 
-    fetch('https://api.jikan.moe/v4/anime?q=${inputValue}')
-    .then((resp)=> resp.json())
-    .then((data)=>{
-        const animeApi = data.data;
-
-        arraySeries=[];
-
-        //bucle sobre el array para obtener datos 
-
-        for(const serie of animeApi) {
-
-        }
-    });
-
-
-
- }
+    fetch(`https://api.jikan.moe/v4/anime?q=${inputText}`)
+    .then(resp => resp.json())
+    .then(info =>{
+        const seriesApi = info.data;
+        renderResults();
+    })
+}
+btn.addEventListener('click', handleClick);
 
